d56009be2fc674c6706a23656e896dda5402720a
gianvittorio <gianvittorio.castellano@gmail.com>
2021-08-05 21:42:41 -0300

    start project

---
+++ src/main/java/com/gianvittorio/aws/lambda/dataqualitychallenge/DataQualityChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gianvittorio.aws.lambda.dataqualitychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataQualityChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataQualityChallengeApplication.class, args);
+	}
+
+}
